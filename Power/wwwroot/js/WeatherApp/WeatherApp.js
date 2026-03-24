213d4d3c810fdd6ac62b843de196a51252e3ec9c
Pavel Korytkin <93835948+pkorytkin@users.noreply.github.com>
2026-03-25 01:07:48 +0300

    Вынос vue кода в отдельный файл.

---
+++ Power/wwwroot/js/WeatherApp/WeatherApp.js
@@ -0,0 +1,45 @@
+﻿const { createApp } = Vue;
+
+createApp({
+    data() {
+        return {
+            tab: 'current',
+            dataIsLoaded: false,
+            forecastData: null,
+            loadInProgress: false
+        }
+    },
+    mounted: function () {
+        this.reloadInfo();
+    },
+    methods: {
+        openCurrent() {
+            this.tab = 'current';
+        },
+        openForecast() {
+            this.tab = 'forecast';
+        },
+        reloadInfo() {
+            if (this.loadInProgress) {
+                return;
+            }
+            this.loadInProgress = true;
+            $.ajax({
+                url: "WeatherAPI/GetWeatherForecast",
+                method: 'get',
+                success: (data) => {
+                    this.dataIsLoaded = true;
+                    this.tab = this.tab == 'error' ? 'current' : this.tab;
+                    this.forecastData = data;
+                    this.loadInProgress = false;
+                },
+                error: (xhr, status, error) => {
+                    this.tab = "error";
+                    this.dataIsLoaded = false;
+                    this.forecastData = null;
+                    this.loadInProgress = false;
+                }
+            });
+        },
+    },
+}).mount('#app');
